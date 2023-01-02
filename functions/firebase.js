100ffdcba9c01903ad91a3b78f1c2a66cd1af3a5
Simon Hildebrandt <simonhildebrandt@gmail.com>
2023-01-02 16:48:37 +1100

    Finished basic functionality

--- functions/firebase.js
+++ functions/firebase.js
@@ -1,19 +1,29 @@
 const { initializeApp } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
+const { datetimeString } = require('firebase-tools/lib/utils');
 
 initializeApp();
 const db = getFirestore();
 
+
+class TeamNotFoundError extends Error {}
+class ResourceNotFoundError extends Error {}
+class BookingNotFoundError extends Error {}
+
+
+
 async function ensureTeam(teamId) {
   const doc = db.collection('teams').doc(teamId);
   const existing = await doc.get();
-  console.log(existing);
   if (existing.exists) {
-    console.log('exists!', existing.data());
     return existing.data();
   } else {
-    doc.set({});
-    return {};
+    const data = {
+      createdAt: new Date().valueOf(),
+      publishingChannel: null
+    };
+    await doc.set(data);
+    return data;
   }
 }
 
@@ -22,8 +32,12 @@ async function createResource(teamId, name) {
   await resources.add({name, deletedAt: null});
 }
 
+function getTeamResourcesQuery(teamId) {
+  return db.collection(`teams/${teamId}/resources`).where('deletedAt', '==', null);
+}
+
 async function getResources(teamId) {
-  const resources = db.collection(`teams/${teamId}/resources`).where('deletedAt', '=', null);
+  const resources = getTeamResourcesQuery(teamId);
   const docs = await resources.get();
   return docs.docs.map(doc => ({...doc.data(), id: doc.id}));
 }
@@ -34,9 +48,80 @@ async function getResource(teamId, resourceId) {
   return {...doc.data(), id: doc.id};
 }
 
+async function getResourceByName(teamId, name) {
+  const query = getTeamResourcesQuery(teamId).where('name', '==', name).limit(1);
+  const docs = await query.get();
+
+  if (docs.docs.length == 1) {
+    const doc = docs.docs[0];
+    return {...doc.data(), id: doc.id};
+  } else {
+    return;
+  }
+}
+
 async function deleteResource(teamId, resourceId) {
   const ref = db.doc(`teams/${teamId}/resources/${resourceId}`);
   const doc = await ref.update({deletedAt: new Date().valueOf()});
+}
+
+async function getUnresolvedBookings(teamId, resource=null) {
+  let bookings = db
+    .collection(`teams/${teamId}/bookings`)
+    .where('resolvedAt', '==', null);
+
+  if (resource) {
+    bookings = bookings.where('resourceId', '==', resource.id);
+  }
+
+  const docs = await bookings.get();
+  return docs.docs.map(doc => ({...doc.data(), id: doc.id}));
+}
+
+async function bookResource(teamId, userId, resource, now) {
+  const actives = await getUnresolvedBookings(teamId, resource);
+  await createResourceBooking(teamId, userId, resource, now, actives.length > 0);
+  return actives;
+}
+
+async function createResourceBooking(teamId, userId, resource, now, existing) {
+  db.collection(`teams/${teamId}/bookings`).add({
+    resourceId: resource.id,
+    userId,
+    createdAt: now,
+    startedAt: existing ? null : now,
+    resolvedAt: null
+  })
+}
+
+async function setPublishingChannel(teamId, channelId) {
+  const doc = db.doc(`teams/${teamId}`);
+  await doc.update({publishingChannel: channelId});
+}
+
+async function updateBooking(teamId, bookingId, data) {
+  await db.doc(`teams/${teamId}/bookings/${bookingId}`).update(data);
+}
+
+async function resolveActiveBooking(teamId, resource) {
+  const bookings = await getUnresolvedBookings(teamId, resource);
+  bookings.sort((a, b) => a.createdAt - b.createdAt)
+
+  console.log({bookings})
+
+  if (bookings.length == 0) {
+    throw new BookingNotFoundError();
+  }
+
+  const next = bookings[0];
+  await updateBooking(teamId, next.id, { resolvedAt: new Date().valueOf() })
+
+  const after = bookings[1];
+  if (after) {
+    await updateBooking(teamId, after.id, { startedAt: new Date().valueOf() })
+  }
+
+  return next;
 }
 
 module.exports = {
@@ -44,5 +129,11 @@ module.exports = {
   createResource,
   getResources,
   getResource,
-  deleteResource
+  getResourceByName,
+  deleteResource,
+  bookResource,
+  getUnresolvedBookings,
+  setPublishingChannel,
+  resolveActiveBooking,
+  BookingNotFoundError
 }
--- functions/index.js
+++ functions/index.js
@@ -7,12 +7,22 @@ const {
   ensureTeam,
   getResources,
   getResource,
+  getResourceByName,
   createResource,
-  deleteResource
+  deleteResource,
+  bookResource,
+  getUnresolvedBookings,
+  setPublishingChannel,
+  resolveActiveBooking,
+  BookingNotFoundError,
 } = require('./firebase');
 
 
 const { App, ExpressReceiver, directMention } = require('@slack/bolt');
+
+
+const APPNAME = "Tesorau";
+
 
 const expressReceiver = new ExpressReceiver({
   signingSecret: config.slack.signing_secret,
@@ -26,58 +36,129 @@ const app = new App({
   processBeforeResponse: true,
 });
 
-// app.client.chat.postMessage({
-//   token: config.slack.bot_token,
-//   channel: "C04AWK55G83",
-//   text: "I've booted up!"
-// });
 
-// React to message.channels event
-const CREATE_REGEX = / create (\S+)/;
+const CREATE_REGEX = / (create|add) (\S+)/;
 app.message(directMention(), CREATE_REGEX, async ({ message, say, context }) => {
   const { teamId } = context;
-  const team = await ensureTeam(teamId);
-  const [_m, name] = message.text.match(CREATE_REGEX);
+  await ensureTeam(teamId);
+  const [_m, _v, name] = message.text.match(CREATE_REGEX);
 
-  // find name, or error
-  // search for existing resource with same name - error
-  // create resource with name
-  await createResource(teamId, name);
+  const existing = await getResourceByName(teamId, name);
+  if (existing) {
+    say(`❌ '${name}' already exists!`);
+  } else {
+    await createResource(teamId, name);
+    say(`✅ ${name} created!`);
+  }
 
-  console.error(message, team);
-  say("Resource created!");
+  await publishStatus(teamId);
+});
+
+const RESOLVE_REGEX = / (resolve|done) (\S+)/;
+app.message(directMention(), RESOLVE_REGEX, async ({ message, say, context }) => {
+  const { teamId } = context;
+  await ensureTeam(teamId);
+  const [_m, _v, name] = message.text.match(RESOLVE_REGEX);
+
+  const existing = await getResourceByName(teamId, name);
+  if (!existing) {
+    say(`❌ resource '${name}' not found.`);
+    return;
+  }
+
+  try {
+    const newActiveBooking = await resolveActiveBooking(teamId, existing);
+    const extra = newActiveBooking ? `\n'${name}' now assigned to <@${newActiveBooking.userId}>.` : '';
+    say(`✅ booking for '${name}' resolved.${extra}`);
+
+    if (newActiveBooking) {
+      app.client.chat.postMessage({
+        token: config.slack.bot_token,
+        channel: newActiveBooking.userId,
+        text: `resource '${name}' is now assigned to you.`
+      });
+    }
+  } catch(err) {
+    if (err instanceof BookingNotFoundError) {
+      say(`❌ booking for resource '${name}' not found.`);
+      return;
+    } else {
+      throw err;
+    }
+  }
+
+  await publishStatus(teamId);
+});
+
+const BOOK_REGEX = / book (\S+)/;
+app.message(directMention(), BOOK_REGEX, async ({ message, say, context }) => {
+
+  const { teamId } = context;
+  await ensureTeam(teamId);
+
+  const { user: userId, text: messageText } = message;
+  const [_m, name] = messageText.match(BOOK_REGEX);
+
+  const existing = await getResourceByName(teamId, name);
+
+  if (!existing) {
+    say(`❌ resource '${name}' not found.`);
+    return;
+  }
+
+  const now = new Date().valueOf();
+  const bookings = await bookResource(teamId, userId, existing, now);
+  const previous = new Intl.ListFormat().format(bookings.map(b => `<@${b.userId}>`));
+
+  if (bookings.length == 0) {
+    say(`✅ ${name} is now yours.`);
+  } else {
+    say(`✅ ${name} booked! You're in the queue behind ${previous}`);
+  }
+
+  await publishStatus(teamId);
 });
 
 const LIST_REGEX = / list/;
 app.message(directMention(), LIST_REGEX, async ({ message, say, context }) => {
-  console.error({context})
-
   const { teamId } = context;
-  const team = await ensureTeam(teamId);
+  await ensureTeam(teamId);
 
   const resources = await getResources(teamId);
+  const bookings = await getUnresolvedBookings(teamId);
 
-  console.error(message, team);
+  function bookingForResource(resourceId) {
+    const booking = bookings.find(b => b.resourceId == resourceId && b.startedAt !== null);
+
+    if (booking) {
+      const { userId } = booking;
+      return `booked by <@${userId}>`
+    } else {
+      return "free"
+    }
+  }
+
   say({
+    text: "Current resource list",
     blocks: [
       {
         type: "section",
         text: {
           type: "mrkdwn",
-          text: "*Current resources*"
+          text: "*Current resource bookings*"
         }
       },
       ...resources.map(({name, id}) => ({
         type: "section",
         text: {
           type: "mrkdwn",
-          text: name
+          text: `*${name}* - ${bookingForResource(id)}`
         },
         accessory: {
           type: "button",
           text: {
             type: "plain_text",
-            text: "Delete"
+            text: "Delete resource"
           },
           action_id: `delete?${id}`
         }
@@ -91,6 +172,8 @@ app.action(DELETE_REQUEST_REGEX, async ({action, ack, say, context}) => {
   await ack();
 
   const { teamId } = context;
+  await ensureTeam(teamId);
+
   const [_m, resourceId] = action.action_id.match(DELETE_REQUEST_REGEX);
   const resource = await getResource(teamId, resourceId);
 
@@ -113,18 +196,143 @@ app.action(DELETE_REQUEST_REGEX, async ({action, ack, say, context}) => {
       }
     ]
   });
-})
+});
+
+const PUBLISH_REGEX = /publish (start|stop)/;
+app.message(directMention(), PUBLISH_REGEX, async ({ message, say, context }) => {
+  const { teamId } = context;
+  const { channel } = message;
+  await ensureTeam(teamId);
+
+  const [_m, verb] = message.text.match(PUBLISH_REGEX);
+
+  const start = verb == 'start';
+
+  console.log({message, context})
+  await setPublishingChannel(teamId, start ? channel : null)
+
+  const text = start ?
+    "Now publishing status to this channel."
+    :
+    "No longer publishing status to this channel.";
+
+  say({ text });
+
+  await publishStatus(teamId);
+});
 
 const DELETE_CONFIRM_REGEX = /delete\!(\w+)/;
 app.action(DELETE_CONFIRM_REGEX, async ({action, ack, respond, context}) => {
   const { teamId } = context;
+  await ensureTeam(teamId);
+
   await ack();
+
   const [_m, resourceId] = action.action_id.match(DELETE_CONFIRM_REGEX);
   const resource = await getResource(teamId, resourceId);
   await deleteResource(teamId, resourceId);
 
-  respond(`${resource.name} deleted!`);
-})
+  respond(`✅ ${resource.name} deleted!`);
+
+  await publishStatus(teamId);
+});
+
+
+const HELP_REGEX = / help/;
+app.message(directMention(), HELP_REGEX, async ({ message, say, context }) => {
+
+  const { teamId, botUserId } = context;
+  await ensureTeam(teamId);
+
+  say({
+    text: "Help message",
+    blocks: [
+      {
+        "type": "header",
+        "text": {
+          "type": "plain_text",
+          "text": `${APPNAME} help`
+        },
+      },
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": `*${APPNAME}* helps you track who's currently using a resource.
+
+Invoke commands using \`<@${botUserId}> <command>\`.
+
+Get started using the following commands:`
+        }
+      },
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": `\`<@${botUserId}> list\` - list all the resources ${APPNAME} is managing, and whether they're booked. (To delete resources, select 'delete resource' in this list.)`
+        }
+      },
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": `\`<@${botUserId}> create <resource name>\` - creates a new resource for the ${APPNAME} bot to manage. (To delete, use the \`list\` command and click 'delete resource'.)`
+        }
+      },
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": `\`<@${botUserId}> book <resource name>\` - books a resource for your use. If the resource is already booked, you'll be added the the queue to use it when it becomes free.`
+        }
+      },
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": `\`<@${botUserId}> resolve <resource name>\` - resolves the active booking for a resource. If there's a queue for the resource, the next person in the queue will be notified, and become the new active booking.`
+        }
+      },
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": `\`<@${botUserId}> publish [start|stop]\` - start or stop publishing in this channel. Publishing involves updating the channel's topic with the list of resources and the current bookings for them.`
+        }
+      },
+    ]
+  });
+});
+
+
+async function publishStatus(teamId) {
+  const { publishingChannel } = await ensureTeam(teamId);
+
+  if (publishingChannel == null) {
+    return;
+  }
+
+  const resources = await getResources(teamId);
+  const bookings = await getUnresolvedBookings(teamId);
+
+  resourceDetails = resources.map(r => {
+    const resBookings = bookings
+      .filter(b => b.resourceId == r.id)
+      .sort((a, b) => b.createdAt - a.createdAt);
+
+    const status = resBookings.map(b => `<@${b.userId}>`).join(" ➡️ ");
+
+    return `${r.name} - ${status || 'free'}`;
+  });
+
+  const topic = resourceDetails.join("\n")
+
+  await app.client.conversations.setTopic({
+    token: config.slack.bot_token,
+    channel: publishingChannel,
+    topic
+  });
+}
 
 // Check the details of the error to handle cases where you should retry sending a message or stop the app
 app.error(console.error);
